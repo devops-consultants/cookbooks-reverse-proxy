24441f2eef7dbff28a2b95bedf893813d8e0d855
Rob Coward <rob.coward@devops-consultants.co.uk>
2018-05-21 11:46:16 +0100

    Use ssl to gateone container

--- recipes/gateone.rb
+++ recipes/gateone.rb
@@ -1,5 +1,5 @@
 haproxy_backend 'gateone' do
-    server ['container0 127.0.0.1:8443 check weight 1 maxconn 100']
+    server ['container0 127.0.0.1:8443 check ssl verify none weight 1 maxconn 100']
 end
 
 node.default['proxy']['acls'] << 'console_host hdr(host) -i console.devops-consultants.com'
